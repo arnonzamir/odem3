2ec9bc895d81a5521e6d52d27a19e6c2098f580f
arnonzamir <arnonzamir@gmail.com>
2023-02-13 21:30:29 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-let mapY = 0
 let y = 0
 let x = 0
+let mapY = 0
 basic.showLeds(`
     # # # # #
     . . # . .
@@ -11,17 +11,24 @@ basic.showLeds(`
 radio.setGroup(1)
 let button = 90
 let mapX = 90
+let forward = true
 basic.forever(function () {
+    mapY = 0
     x = pins.analogReadPin(AnalogPin.P1)
-    mapX = Math.map(x, 0, 1023, 0, 45)
+    mapX = Math.map(x, 0, 1023, 0, 255)
     radio.sendValue("x", mapX)
-    serial.writeValue("x", x)
-    serial.writeValue("mapX", mapX)
-    y = pins.analogReadPin(AnalogPin.P2)
-    mapY = Math.map(y, 0, 1023, 0, 255)
-    radio.sendValue("y", mapY)
-    serial.writeValue("y", y)
+    y = pins.analogReadPin(AnalogPin.P1)
+    forward = y > 512
+    if (y < 510) {
+        y = 512 - y
+    } else if (y > 514) {
+        y = y - 512
+    } else {
+        y = 0
+    }
+    mapY = Math.map(y, 0, 512, 0, 255)
     serial.writeValue("mapY", mapY)
+    radio.sendValue("y", mapY)
     if (input.buttonIsPressed(Button.A)) {
         button += 5
         serial.writeValue("button_state", button)
